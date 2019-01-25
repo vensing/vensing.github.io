81aa813502aee7e689eeb5d64acdfda34b76fe0a
vensing <782176881@qq.com>
2019-01-25 23:17:05 +0800

    Travis CI

---
+++ themes/hexo-theme-snippet/scripts/process.js
@@ -0,0 +1,6 @@
+
+hexo.on('ready', function () {
+  hexo.log.info("=========================================");
+  hexo.log.info("  Welcome to use Snippet theme for hexo  ");
+  hexo.log.info("=========================================");
+});
