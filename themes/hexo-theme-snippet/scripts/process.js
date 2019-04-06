5342a145b29384b3e9c72beee0176cba74a8d682
vensing <vensing@foxmail.com>
2019-04-06 20:23:50 +0800

    add logo

--- themes/hexo-theme-snippet/scripts/process.js
+++
@@ -1,6 +0,0 @@
-
-hexo.on('ready', function () {
-  hexo.log.info("=========================================");
-  hexo.log.info("  Welcome to use Snippet theme for hexo  ");
-  hexo.log.info("=========================================");
-});
